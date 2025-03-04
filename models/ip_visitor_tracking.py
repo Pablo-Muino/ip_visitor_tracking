335426ced065157ec827ef2e1430b7dd3981ab1d
Pablo Muiño <pabmuirod@gmail.com>
2025-03-04 09:51:36 +0100

    Cambiadas las clases ir.model.access.csv y ip_visitor_tracking.py

--- models/ip_visitor_tracking.py
+++ models/ip_visitor_tracking.py
@@ -6,7 +6,7 @@ import logging
 # Configuramos el logger
 _logger = logging.getLogger(__name__)
 
-class IpVisitorTracker(models.Model):
+class IpVisitorTracking(models.Model):
     _name = 'ip.visitor.tracker'
     _description = 'Tracker del cliente mediante su Ip'
     _rec_name = 'ip_address'
